3b900c54f327449f5dd0db985b3a0e326a3522b3
Angel Moreno <jangelmorenol@gmail.com>
2015-11-04 10:15:34 -0600

    Missed AccountContext files

---
+++ LoginTestApp.Repository.Contracts/IAccountContext.cs
@@ -0,0 +1,15 @@
+﻿using LoginTestApp.Repository.Contracts.Repositories;
+
+namespace LoginTestApp.Repository.Contracts
+{
+    public interface IAccountContext : IContext
+    {
+        IUsersRepository Users { get; }
+
+        IDynamicLinksRepository DynamicLinks { get; }
+
+        #region Methods
+
+        #endregion Methods
+    }
+}
---
+++ LoginTestApp.Repository/AccountContext.cs
@@ -0,0 +1,24 @@
+using System.Data.Entity;
+using LoginTestApp.Crosscutting.Contracts;
+using LoginTestApp.DataAccess.Contracts.Context;
+using LoginTestApp.Repository.Contracts;
+using LoginTestApp.Repository.Contracts.Repositories;
+
+namespace LoginTestApp.Repository
+{
+    public class AccountContext : ContextBase, IAccountContext
+    {
+        public AccountContext(ILoginTestAppContext dbContext, IDataMapper dataMapper) 
+            : base((DbContext)dbContext, dataMapper)
+        {
+        }
+
+        #region Repositories
+
+        public IUsersRepository Users => GetRepository<IUsersRepository>();
+
+        public IDynamicLinksRepository DynamicLinks => GetRepository<IDynamicLinksRepository>();
+
+        #endregion Repositories
+    }
+}
